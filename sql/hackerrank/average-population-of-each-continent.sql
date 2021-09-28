086840d93dfe3959d34fed2483699072caf5b7d3
Jithendra Yenugula <jithendra1230@gmail.com>
2021-09-28 21:55:13 +0530

    Update average-population-of-each-continent.sql

--- sql/hackerrank/average-population-of-each-continent.sql
+++ sql/hackerrank/average-population-of-each-continent.sql
@@ -3,5 +3,6 @@
 
 SELECT country.continent, FLOOR(AVG(city.population))
 FROM city
-INNER JOIN country ON city.countrycode = country.code
-GROUP BY country.continent;
+INNER JOIN country 
+ON city.countrycode = country.code
+GROUP BY country.continent
